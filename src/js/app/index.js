7e61b2a1ff06961dbe206ee8dafa79874b8f7ce2
sezzh <sezzhltd@gmail.com>
2016-12-31 17:27:54 -0600

    todo app example with 2 actions in the reducer without react

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -15,6 +15,13 @@ import deepFreeze from 'deep-freeze'
           completed: false
         }
       ]
+    } else if (action.type === 'TOGGLE_TODO') {
+      return state.map((todo) => {
+        if (todo.id !== action.id) {
+          return todo
+        }
+        return Object.assign({}, todo, { completed: !todo.completed })
+      })
     } else {
       return state
     }
@@ -45,6 +52,47 @@ import deepFreeze from 'deep-freeze'
     ).toEqual(stateAfter)
   }
 
+  const testToggleTodo = () => {
+    const stateBefore = [
+      {
+        id: 0,
+        text: 'Learn Redux',
+        completed: false
+      },
+      {
+        id: 1,
+        text: 'Go shopping',
+        completed: false
+      }
+    ]
+
+    const action = {
+      type: 'TOGGLE_TODO',
+      id: 1
+    }
+
+    const stateAfter = [
+      {
+        id: 0,
+        text: 'Learn Redux',
+        completed: false
+      },
+      {
+        id: 1,
+        text: 'Go shopping',
+        completed: true
+      }
+    ]
+
+    deepFreeze(stateBefore)
+    deepFreeze(action)
+
+    expect(
+      todos(stateBefore, action)
+    ).toEqual(stateAfter)
+  }
+
   testAddTodo()
+  testToggleTodo()
   console.log('All test passed')
 })()
