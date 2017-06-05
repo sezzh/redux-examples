2993d2f2df86bb0fe695cbb32fb9e2f3a25aa899
Jesús Cruz <jesus.cruz@sngular.team>
2017-06-05 08:04:52 -0500

    added toggle functionality

--- src/js/app/index.jsx
+++ src/js/app/index.jsx
@@ -37,7 +37,17 @@ import deepFreeze from 'deep-freeze'
           </button>
           <ul>
             {this.props.todos.map((todo) => {
-              return <li key={todo.id}>{todo.text}</li>
+              return <li
+                key={todo.id}
+                onClick={() => {
+                  store.dispatch({
+                    type: 'TOGGLE_TODO',
+                    id: todo.id
+                  })
+                }}
+                style={{
+                  textDecoration: todo.completed ? 'line-through' : 'none'
+                }}>{todo.text}</li>
             })}
           </ul>
         </div>
