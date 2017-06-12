cc22dcbf60c97a123eabda21956852b0637ff2e0
Jesús Cruz <jesus.cruz@sngular.team>
2017-06-11 19:49:55 -0500

    added container composition with react and redux in the whole app

--- src/js/app/index.jsx
+++ src/js/app/index.jsx
@@ -7,19 +7,66 @@ import deepFreeze from 'deep-freeze'
 (function () {
   let nextTodoId = 0
 
-  // This is another react component but so much simple!
-  const Link = ({active, children, onFilterClick}) => {
-    if (active) {
-      return (<span>{children}</span>)
+  /** START REDUCERS */
+
+  // todo reducer function
+  const todo = (state, action) => {
+    if (action.type === 'ADD_TODO') {
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false
+      }
+    } else if (action.type === 'TOGGLE_TODO') {
+      if (state.id !== action.id) {
+        return state
+      }
+      return Object.assign({}, state, { completed: !state.completed })
+    } else {
+      return state
     }
-    return (
-      <a href='#' onClick={(event) => {
-        event.preventDefault()
-        onFilterClick()
-      }}>{children}</a>
-    )
   }
 
+  // todo list reducer function
+  const todos = (state = [], action) => {
+    if (action.type === 'ADD_TODO') {
+      return [
+        ...state,
+        todo(state, action)
+      ]
+    } else if (action.type === 'TOGGLE_TODO') {
+      return state.map((t) => todo(t, action))
+    } else {
+      return state
+    }
+  }
+
+  // filter reducer function
+  const visibilityFilter = (state = 'SHOW_ALL', action) => {
+    if (action.type === 'SET_VISIBILITY_FILTER') {
+      return action.filter
+    } else {
+      return state
+    }
+  }
+
+  // Principal Reducer function.
+  const todoApp = combineReducers({ // This is a good practice.
+    todos,
+    visibilityFilter
+  })
+
+/** END REDUCERS */
+
+  // Creating the REDUX store.
+  const store = createStore(
+    todoApp,
+    // Setting up the redux dev tools.
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+
+
+/** STARTS React functionality containers */
   /**
    * This is a container.
    * In the render method it calculates if the link component should be
@@ -55,6 +102,49 @@ import deepFreeze from 'deep-freeze'
     }
   }
 
+  class VisibleTodoList extends React.Component {
+    componentDidMount () {
+      this.unsubscribe = store.subscribe(() => {
+        this.forceUpdate()
+      })
+    }
+
+    componentWillUnmount () {
+      this.unsubscribe()
+    }
+
+    render () {
+      const props = this.props
+      const state = store.getState()
+      return (
+        <TodoList
+          todos={getVisibleTodos(state.todos, state.visibilityFilter)}
+          onTodoClick={(id) => {
+            store.dispatch({
+              type: 'TOGGLE_TODO',
+              id: id
+            })
+          }} />
+      )
+    }
+  }
+
+/** ENDS React functionality containers */
+/** STARTS PRESENTATIONAL COMPONENTS */
+
+  // This is another react component but so much simple!
+  const Link = ({active, children, onFilterClick}) => {
+    if (active) {
+      return (<span>{children}</span>)
+    }
+    return (
+      <a href='#' onClick={(event) => {
+        event.preventDefault()
+        onFilterClick()
+      }}>{children}</a>
+    )
+  }
+
   const Todo = ({onClick, completed, text}) => (
     <li
       onClick={onClick}
@@ -74,7 +164,7 @@ import deepFreeze from 'deep-freeze'
     </ul>
   )
 
-  const AddTodo = ({onAddClick}) => {
+  const AddTodo = () => {
     let input
     return (
       <div>
@@ -82,7 +172,11 @@ import deepFreeze from 'deep-freeze'
           input = node
         }} />
         <button onClick={() => {
-          onAddClick(input.value)
+          store.dispatch({
+            type: 'ADD_TODO',
+            id: nextTodoId++,
+            text: input.value
+          })
           input.value = ''
         }}>
         Add Todo
@@ -111,35 +205,19 @@ import deepFreeze from 'deep-freeze'
     </p>
   )
 
-  const TodoApp = ({todos, visibilityFilter}) => {
-    const visibleTodos = getVisibleTodos(
-      todos,
-      visibilityFilter
-    )
+  const TodoApp = () => {
     return (
       <div>
-        <AddTodo
-          onAddClick={(text) => {
-            store.dispatch({
-              type: 'ADD_TODO',
-              id: nextTodoId++,
-              text: text
-            })
-          }}
-        />
-        <TodoList
-          todos={visibleTodos} onTodoClick={(id) => {
-            store.dispatch({
-              type: 'TOGGLE_TODO',
-              id: id
-            })
-          }}
-        />
+        <AddTodo />
+        <VisibleTodoList />
         <Footer />
       </div>
     )
   }
 
+/** ENDS PRESENTATIONAL COMPONENTS */
+
+  // Filter function
   const getVisibleTodos = (todos, filter) => {
     switch (filter) {
       case 'SHOW_ALL':
@@ -154,70 +232,15 @@ import deepFreeze from 'deep-freeze'
     }
   }
 
-  // reducer function
-  const todo = (state, action) => {
-    if (action.type === 'ADD_TODO') {
-      return {
-        id: action.id,
-        text: action.text,
-        completed: false
-      }
-    } else if (action.type === 'TOGGLE_TODO') {
-      if (state.id !== action.id) {
-        return state
-      }
-      return Object.assign({}, state, { completed: !state.completed })
-    } else {
-      return state
-    }
-  }
-
-  // reducer function
-  const todos = (state = [], action) => {
-    if (action.type === 'ADD_TODO') {
-      return [
-        ...state,
-        todo(state, action)
-      ]
-    } else if (action.type === 'TOGGLE_TODO') {
-      return state.map((t) => todo(t, action))
-    } else {
-      return state
-    }
-  }
-
-  // reducer function
-  const visibilityFilter = (state = 'SHOW_ALL', action) => {
-    if (action.type === 'SET_VISIBILITY_FILTER') {
-      return action.filter
-    } else {
-      return state
-    }
-  }
-
-  // Principal Reducer function.
-  const todoApp = combineReducers({ // This is a good practice.
-    todos,
-    visibilityFilter
-  })
-
-  const render = () => {
-    ReactDOM.render(
-      // Render the TodoApp component to the <dev> with id 'root'
-      <TodoApp
-        todos={store.getState().todos}
-        visibilityFilter={store.getState().visibilityFilter} />,
-      document.querySelector('#foo')
-    )
-  }
-  const store = createStore(
-    todoApp,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // Rendering for the first time the app.
+  ReactDOM.render(
+    // Render the TodoApp component to the <dev> with id 'root'
+    <TodoApp />, document.querySelector('#foo')
   )
-  store.subscribe(render)
+
+  // just a little store sub for the console.
   store.subscribe(() => {
     console.log('Current state: ')
     console.log(store.getState())
   })
-  render()
 })()
